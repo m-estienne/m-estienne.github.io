5262245e6dc08e914c23ab1c21ce0213ec162f18
m-estienne <estienne.mathieu@gmail.com>
2023-01-03 14:48:54 +0100

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,32 @@
 const btn = document.querySelector('#menu');
 const nav = document.querySelector('.navbar');
+const header = document.querySelector('header');
+const onScroll = document.querySelector('.onScroll');
+let backgroundImg = ['./assets/img/background.jpg', './assets/img/background2.jpg', './assets/img/background3.jpg'];
+let imageIndex = 0;
 
-btn.addEventListener('click', navbarClick);
+/*Changer l'image de fond du header*/ 
+function changeImage() {
+    header.style.backgroundImage = 'url('+backgroundImg[imageIndex]+')';
+  imageIndex++;
+  if (imageIndex >= backgroundImg.length) {
+    imageIndex = 0;
+  }
+}
 
 //Afficher et masquer la navbar au click sur le bouton
 function navbarClick() {
     nav.classList.toggle('hide')
 };
+
+//charger la suite de la page au scroll
+function loadOnScroll() {
+    if(scrollY >= 950){
+    onScroll.classList.remove('hide');
+    }
+}
+
+setInterval(changeImage,15000);
+document.addEventListener('scroll', loadOnScroll);
+btn.addEventListener('click', navbarClick);
+
